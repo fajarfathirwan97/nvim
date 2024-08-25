3a25e8d07b39f9f3286b251cb9ec515f9b8e1480
fajarfathirwan <fajarfathirwan97@gmail.com>
2024-08-25 23:37:58 +0700

    add color scheme

--- lua/plugins/astrocommunity.lua
+++ lua/plugins/astrocommunity.lua
@@ -1,6 +1,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.recipes.telescope-nvchad-theme" },
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
    { import = "astrocommunity.recipes.telescope-lsp-mappings" },
 }
