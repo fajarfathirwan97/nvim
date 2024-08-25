71e1aae21c634fbab4c0e5af0f7fc1698c468bad
fajarfathirwan <fajarfathirwan97@gmail.com>
2024-08-25 18:37:48 +0700

    transparent bg

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -198,6 +198,10 @@ return {
   },
 
   {
+    "crusoexia/vim-monokai"
+  },
+
+  {
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
       require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
